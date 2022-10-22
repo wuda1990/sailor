95b38943327d00439d88d09b6be2530e2eaf3f4e
huajun.wu <huajun.wu@aihuishou.com>
2022-10-22 10:06:37 +0800

    Q45

--- src/main/java/com/onepiece/leetcode/editor/en/Q45JumpGameIi.java
+++ src/main/java/com/onepiece/leetcode/editor/en/Q45JumpGameIi.java
@@ -48,7 +48,7 @@ package com.onepiece.leetcode.editor.en;
     //编号：[45]
       public static void main(String[] args) {
            Solution solution = new Q45JumpGameIi().new Solution();
-          System.out.println(solution.jump2(new int[]{2, 3, 1, 1, 4}));
+          System.out.println(solution.jump(new int[]{2, 3, 1, 1, 4}));
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
