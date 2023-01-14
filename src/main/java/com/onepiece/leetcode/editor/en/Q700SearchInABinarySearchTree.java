00aec490c85d4f1fc382c46d7794cb914b71464d
huajun.wu <huajun.wu@aihuishou.com>
2023-01-14 16:20:08 +0800

    binary tree

---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q700SearchInABinarySearchTree.java
@@ -0,0 +1,87 @@
+//You are given the root of a binary search tree (BST) and an integer val. 
+//
+// Find the node in the BST that the node's value equals val and return the subt
+//ree rooted with that node. If such a node does not exist, return null. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [4,2,7,1,3], val = 2
+//Output: [2,1,3]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [4,2,7,1,3], val = 5
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [1, 5000]. 
+// 1 <= Node.val <= 107 
+// root is a binary search tree. 
+// 1 <= val <= 107 
+// 
+// Related Topics Tree Binary Search Tree Binary Tree 
+// 👍 4458 👎 163
+
+
+  package com.onepiece.leetcode.editor.en;
+
+import com.onepiece.leetcode.editor.TreeNode;
+
+public class Q700SearchInABinarySearchTree{
+ //2023-01-14 18:42:37
+    //Search in a Binary Search Tree
+    //编号：[700]
+      public static void main(String[] args) {
+           Solution solution = new Q700SearchInABinarySearchTree().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+//        TreeNode leftTree = searchBST(root.left, val);
+//        if (leftTree != null) {
+//            return leftTree;
+//        }
+//        TreeNode rightTree = searchBST(root.right, val);
+//        if (rightTree != null) {
+//            return rightTree;
+//        }
+//        return null;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q98ValidateBinarySearchTree.java
@@ -0,0 +1,99 @@
+//Given the root of a binary tree, determine if it is a valid binary search tree
+// (BST). 
+//
+// A valid BST is defined as follows: 
+//
+// 
+// The left subtree of a node contains only nodes with keys less than the node's
+// key. 
+// The right subtree of a node contains only nodes with keys greater than the no
+//de's key. 
+// Both the left and right subtrees must also be binary search trees. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [2,1,3]
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [5,1,4,null,null,3,6]
+//Output: false
+//Explanation: The root node's value is 5 but its right child's value is 4.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [1, 104]. 
+// -231 <= Node.val <= 231 - 1 
+// 
+// Related Topics Tree Depth-First Search Binary Search Tree Binary Tree 
+// 👍 13586 👎 1109
+
+
+  package com.onepiece.leetcode.editor.en;
+
+import com.onepiece.leetcode.editor.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Q98ValidateBinarySearchTree{
+ //2023-01-14 19:06:35
+    //Validate Binary Search Tree
+    //编号：[98]
+      public static void main(String[] args) {
+           Solution solution = new Q98ValidateBinarySearchTree().new Solution();
+//          TreeNode root = TreeNode.build(new String[]{"5", "4", "6", "null", "null", "3", "7"});
+//          TreeNode root = TreeNode.build(new String[]{"2", "2", "2"});
+          TreeNode root = TreeNode.build(new String[]{"2147483647"});
+          boolean validBST = solution.isValidBST(root);
+          System.out.println(validBST);
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValid(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        if (!isValid(root.left, min, root.val)) {
+            return false;
+        }
+        if (!isValid(root.right, root.val, max)) {
+            return false;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
