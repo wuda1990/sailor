5d016895905a96b4d8fb7b254dd5dba62ab6f79c
huajun.wu <huajun.wu@aihuishou.com>
2023-01-19 13:44:53 +0800

    backtrack

---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q40CombinationSumIi.java
@@ -0,0 +1,65 @@
+//Given a collection of candidate numbers (candidates) and a target number (targ
+//et), find all unique combinations in candidates where the candidate numbers sum 
+//to target. 
+//
+// Each number in candidates may only be used once in the combination. 
+//
+// Note: The solution set must not contain duplicate combinations. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: candidates = [10,1,2,7,6,1,5], target = 8
+//Output: 
+//[
+//[1,1,6],
+//[1,2,5],
+//[1,7],
+//[2,6]
+//]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: candidates = [2,5,2,1,2], target = 5
+//Output: 
+//[
+//[1,2,2],
+//[5]
+//]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= candidates.length <= 100 
+// 1 <= candidates[i] <= 50 
+// 1 <= target <= 30 
+// 
+// Related Topics Array Backtracking 
+// 👍 7716 👎 191
+
+
+package com.onepiece.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q40CombinationSumIi{
+    public static void main(String[]args){
+        Solution solution=new Q40CombinationSumIi().new Solution();
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
