c57b5c20e3af99bf55255fd8cffb0d11fb6bb67b
huajun.wu <huajun.wu@aihuishou.com>
2023-01-30 07:34:37 +0800

    dynamic programming

--- src/main/java/com/onepiece/leetcode/editor/en/Q343IntegerBreak.java
+++ src/main/java/com/onepiece/leetcode/editor/en/Q343IntegerBreak.java
@@ -46,10 +46,9 @@ public class Q343IntegerBreak {
     class Solution {
         public int integerBreak(int n) {
             int[] dp = new int[n + 1];
-            dp[0] = 0;
-            dp[1] = 0;
-            for (int i = 2; i <= n; i++) {
-                for (int j = i; j > 0; j--) {
+            dp[2] = 1;
+            for (int i = 3; i <= n; i++) {
+                for (int j = 1; j <= i / 2; j++) {
                     dp[i] = Math.max(dp[i], j * Math.max(i - j, dp[i - j]));
                 }
             }
