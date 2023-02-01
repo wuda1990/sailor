82194a2468113d3d4b2940b274266f82849e8a90
huajun.wu <huajun.wu@aihuishou.com>
2023-02-01 18:52:53 +0800

    dynamic programming

---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q377CombinationSumIv.java
@@ -0,0 +1,77 @@
+//Given an array of distinct integers nums and a target integer target, return t
+//he number of possible combinations that add up to target. 
+//
+// The test cases are generated so that the answer can fit in a 32-bit integer. 
+//
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,2,3], target = 4
+//Output: 7
+//Explanation:
+//The possible combination ways are:
+//(1, 1, 1, 1)
+//(1, 1, 2)
+//(1, 2, 1)
+//(1, 3)
+//(2, 1, 1)
+//(2, 2)
+//(3, 1)
+//Note that different sequences are counted as different combinations.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [9], target = 3
+//Output: 0
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 200 
+// 1 <= nums[i] <= 1000 
+// All the elements of nums are unique. 
+// 1 <= target <= 1000 
+// 
+//
+// 
+// Follow up: What if negative numbers are allowed in the given array? How does 
+//it change the problem? What limitation we need to add to the question to allow n
+//egative numbers? 
+// Related Topics Array Dynamic Programming 
+// 👍 5456 👎 560
+
+
+package com.onepiece.leetcode.editor.en;
+
+public class Q377CombinationSumIv{
+    public static void main(String[]args){
+        Solution solution=new Q377CombinationSumIv().new Solution();
+        solution.combinationSum4(new int[]{1, 2, 3}, 4);
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int j = 0; j <= target; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (j >= nums[i]) {
+                    dp[j] = dp[j] + dp[j - nums[i]];
+                }
+                System.out.print(dp[j] + ",");
+            }
+            System.out.println();
+        }
+        return dp[target];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q494TargetSum.java
@@ -0,0 +1,97 @@
+//You are given an integer array nums and an integer target. 
+//
+// You want to build an expression out of nums by adding one of the symbols '+' 
+//and '-' before each integer in nums and then concatenate all the integers. 
+//
+// 
+// For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 
+//and concatenate them to build the expression "+2-1". 
+// 
+//
+// Return the number of different expressions that you can build, which evaluate
+//s to target. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,1,1,1,1], target = 3
+//Output: 5
+//Explanation: There are 5 ways to assign symbols to make the sum of nums be tar
+//get 3.
+//-1 + 1 + 1 + 1 + 1 = 3
+//+1 - 1 + 1 + 1 + 1 = 3
+//+1 + 1 - 1 + 1 + 1 = 3
+//+1 + 1 + 1 - 1 + 1 = 3
+//+1 + 1 + 1 + 1 - 1 = 3
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1], target = 1
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 20 
+// 0 <= nums[i] <= 1000 
+// 0 <= sum(nums[i]) <= 1000 
+// -1000 <= target <= 1000 
+// 
+// Related Topics Array Dynamic Programming Backtracking 
+// 👍 8733 👎 311
+
+
+package com.onepiece.leetcode.editor.en;
+
+public class Q494TargetSum {
+    public static void main(String[] args) {
+        Solution solution = new Q494TargetSum().new Solution();
+//        solution.findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3);
+        solution.findTargetSumWays(new int[]{100}, -200);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * x 是取正数的和,有
+     * x - (sum-x) = target
+     * x == (sum+target)/2
+     * 转化成0，1背包问题,在n个物品里,是否选择+1，凑满容量为x的组合个数
+     */
+    class Solution {
+        public int findTargetSumWays(int[] nums, int target) {
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            //排除无解情况
+            if (Math.abs(target) > sum || (sum + target) % 2 == 1) {
+                return 0;
+            }
+            int x = Math.abs((sum + target) / 2);
+            //dp[i][j]表示从(0,i)个物品里，能够凑满容量为j里有多少种组合个数
+            int[][] dp = new int[nums.length][x + 1];
+            dp[0][0] = 1;
+            for (int j = x; j >= nums[0]; j--) {
+                dp[0][j] += dp[0][j - nums[0]];
+            }
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j <= x; j++) {
+                    if (j >= nums[i]) {
+                        dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]];
+                    }else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+
+                }
+            }
+            return dp[nums.length - 1][x];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
