ffd86bb1a2c6b96e8a80b7476d3c25c4035f7534
huajun.wu <huajun.wu@aihuishou.com>
2023-03-17 17:19:00 +0800

    栈与队列二刷

---
+++ src/main/java/com/onepiece/leetcode/editor/en/Q208ImplementTriePrefixTree.java
@@ -0,0 +1,86 @@
+//A trie (pronounced as "try") or prefix tree is a tree data structure used to 
+//efficiently store and retrieve keys in a dataset of strings. There are various 
+//applications of this data structure, such as autocomplete and spellchecker. 
+//
+// Implement the Trie class: 
+//
+// 
+// Trie() Initializes the trie object. 
+// void insert(String word) Inserts the string word into the trie. 
+// boolean search(String word) Returns true if the string word is in the trie (
+//i.e., was inserted before), and false otherwise. 
+// boolean startsWith(String prefix) Returns true if there is a previously 
+//inserted string word that has the prefix prefix, and false otherwise. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input
+//["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+//[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+//Output
+//[null, null, true, false, true, null, true]
+//
+//Explanation
+//Trie trie = new Trie();
+//trie.insert("apple");
+//trie.search("apple");   // return True
+//trie.search("app");     // return False
+//trie.startsWith("app"); // return True
+//trie.insert("app");
+//trie.search("app");     // return True
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= word.length, prefix.length <= 2000 
+// word and prefix consist only of lowercase English letters. 
+// At most 3 * 10⁴ calls in total will be made to insert, search, and 
+//startsWith. 
+// 
+//
+// Related Topics Hash Table String Design Trie 👍 9357 👎 113
+
+
+package com.onepiece.leetcode.editor.en;
+
+public class Q208ImplementTriePrefixTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Trie {
+
+        public Trie() {
+
+        }
+
+        public void insert(String word) {
+
+        }
+
+        public boolean search(String word) {
+            return true;
+        }
+
+        public boolean startsWith(String prefix) {
+            return true;
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/onepiece/leetcode/editor/en/stackQueue/Q347TopKFrequentElements.java
@@ -0,0 +1,96 @@
+//Given an integer array nums and an integer k, return the k most frequent 
+//elements. You may return the answer in any order. 
+//
+// 
+// Example 1: 
+// Input: nums = [1,1,1,2,2,3], k = 2
+//Output: [1,2]
+// 
+// Example 2: 
+// Input: nums = [1], k = 1
+//Output: [1]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// k is in the range [1, the number of unique elements in the array]. 
+// It is guaranteed that the answer is unique. 
+// 
+//
+// 
+// Follow up: Your algorithm's time complexity must be better than O(n log n), 
+//where n is the array's size. 
+//
+// Related Topics Array Hash Table Divide and Conquer Sorting Heap (Priority 
+//Queue) Bucket Sort Counting Quickselect 👍 12951 👎 475
+
+
+package com.onepiece.leetcode.editor.en.stackQueue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q347TopKFrequentElements {
+
+    public static void main(String[] args) {
+        Solution solution = new Q347TopKFrequentElements().new Solution();
+//        solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+        solution.topKFrequent(new int[]{-1, -1}, 1);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int[] topKFrequent(int[] nums, int k) {
+            int[] count = new int[20001];
+            for (int i : nums) {
+                count[i + 10000]++;
+            }
+            PriorityQueue<Pair> priorityQueue = new PriorityQueue<>(nums.length,
+                Comparator.comparingInt(Pair::getRight).reversed());
+
+            for (int i = 0; i < count.length; i++) {
+                if (count[i] > 0) {
+                    priorityQueue.offer(new Pair(i - 10000, count[i]));
+                }
+            }
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++) {
+                result[i] = priorityQueue.poll().getLeft();
+            }
+            return result;
+        }
+
+        class Pair {
+
+            int left;
+            int right;
+
+            public Pair(final int left, final int right) {
+                this.left = left;
+                this.right = right;
+            }
+
+            public int getLeft() {
+                return left;
+            }
+
+            public void setLeft(final int left) {
+                this.left = left;
+            }
+
+            public int getRight() {
+                return right;
+            }
+
+            public void setRight(final int right) {
+                this.right = right;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
