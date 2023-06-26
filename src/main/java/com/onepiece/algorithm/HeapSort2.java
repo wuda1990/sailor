7a94303df9b99ebf43cb84356c7821d75e070b90
huajun.wu <huajun.wu@aihuishou.com>
2023-06-26 15:23:55 +0800

    review heap sort

--- src/main/java/com/onepiece/algorithm/HeapSort2.java
+++ src/main/java/com/onepiece/algorithm/HeapSort2.java
@@ -1,6 +1,7 @@
 package com.onepiece.algorithm;
 
 import java.util.Arrays;
+
 public class HeapSort2 {
 
     private int[] arr;
@@ -15,14 +16,14 @@ public class HeapSort2 {
     public void sort() {
         /*
          *  第一步：将数组堆化
-         *  beginIndex = 第一个非叶子节点。
+         *  heapifyBegin = 第一个非叶子节点。
          *  从第一个非叶子节点开始即可。无需从最后一个叶子节点开始。
          *  叶子节点可以看作已符合堆要求的节点，根节点就是它自己且自己以下值为最大。
          */
-        int len = arr.length - 1;
-        int beginIndex = (len - 1) >> 1;
-        for (int i = beginIndex; i >= 0; i--) {
-            maxHeapify(i, len);
+        int lastIndex = arr.length - 1;
+        int heapifyBegin = lastIndex >> 1;
+        for (int i = heapifyBegin; i >= 0; i--) {
+            maxHeapify(i, lastIndex);
         }
 
         /*
@@ -31,7 +32,7 @@ public class HeapSort2 {
          * 然后从新整理被换到根节点的末尾元素，使其符合堆的特性。
          * 直至未排序的堆长度为 0。
          */
-        for (int i = len; i > 0; i--) {
+        for (int i = lastIndex; i > 0; i--) {
             swap(0, i);
             maxHeapify(0, i - 1);
         }
@@ -54,9 +55,13 @@ public class HeapSort2 {
         int ri = li + 1;           // 右子节点索引
         int cMax = li;             // 子节点值最大索引，默认左子节点。
 
-        if (li > len) return;       // 左子节点索引超出计算范围，直接返回。
+        if (li > len) {
+            return;       // 左子节点索引超出计算范围，直接返回。
+        }
         if (ri <= len && arr[ri] > arr[li]) // 先判断左右子节点，哪个较大。
+        {
             cMax = ri;
+        }
         if (arr[cMax] > arr[index]) {
             swap(cMax, index);      // 如果父节点被子节点调换，
             maxHeapify(cMax, len);  // 则需要继续判断换下后的父节点是否符合堆的特性。
@@ -64,14 +69,37 @@ public class HeapSort2 {
     }
 
     /**
+     * @param k: the Kth largest element in array
+     * @return
+     */
+    public int findKthLargest(int k) {
+        //数组堆化
+        int lastIndex = arr.length - 1;
+        int heapifyBegin = lastIndex >> 1;
+        for (int i = heapifyBegin; i >= 0; i--) {
+            maxHeapify(i, lastIndex);
+        }
+        //堆化操作k-1次，此时堆顶就是第k大的元素
+        for (int i = 0; i < k - 1; i++) {
+            //第0个元素最大，交换到尾部，类似选择排序
+            swap(0, lastIndex - i);
+            maxHeapify(0, lastIndex - 1 - i);
+        }
+        return arr[0];
+    }
+
+    /**
      * 测试用例
      * <p>
-     * 输出：
-     * [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9]
+     * 输出： [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9]
      */
     public static void main(String[] args) {
         int[] arr = new int[]{3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6};
         new HeapSort2(arr).sort();
         System.out.println(Arrays.toString(arr));
+
+        // test findKthLargest
+        int kthLargest = new HeapSort2(new int[]{3, 7, 87, 66, 5, 69, 22}).findKthLargest(1);
+        System.out.println(kthLargest);
     }
 }
